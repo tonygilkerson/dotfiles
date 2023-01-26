d3da5b3d67846cf2dac4f18bf2dd066abd4f2405
Tony Gilkerson <tgilkerson@miletwo.us>
2023-01-26 09:58:13 -0500

    use all in one kubeconfig

--- kubeconf/load-k8s-config.sh
+++ kubeconf/load-k8s-config.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
-# If there's already a kubeconfig file in ~/.kube/config it will import that too and all the contexts
-DEFAULT_KUBECONFIG_FILE="$HOME/.kube/config"
-if test -f "${DEFAULT_KUBECONFIG_FILE}"
-then
-  export KUBECONFIG="$DEFAULT_KUBECONFIG_FILE"
-fi
+# exit when any command fails
+set -e
+# set -x			# activate debugging from here
+# set +x			# stop debugging from here
+
+#
+# Use the default  ~/.kube/config, it will import that too and all the contexts
+#
+export KUBECONFIG="$HOME/.kube/config"
+
+#
 # Your additional kubeconfig files should be inside ~/.kube/config-files
+#
 ADD_KUBECONFIG_FILES="$HOME/.kube/config-files"
 mkdir -p "${ADD_KUBECONFIG_FILES}"
 OIFS="$IFS"
 IFS=$'\n'
-for kubeconfigFile in `find "${ADD_KUBECONFIG_FILES}" -type f -name "*.yml" -o -name "*.yaml"`
+for kubeconfigFile in $(find "${ADD_KUBECONFIG_FILES}" -type f -name "*.yml" -o -name "*.yaml")
 do
     export KUBECONFIG="$kubeconfigFile:$KUBECONFIG"
 done
 IFS="$OIFS"
+
+#
+# Merg them all into one and use the one
+#
+cp "$HOME/.kube/config" "$HOME/.kube/config-$(date '+%Y-%m-%d-%H:%M:%S')"
+kubectl config view --flatten > "$HOME/.kube/merged-config" && mv "$HOME/.kube/merged-config" "$HOME/.kube/config"
+export KUBECONFIG="$HOME/.kube/config"
