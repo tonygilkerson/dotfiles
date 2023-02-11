4bd2e861c1e774e88ada857b789b07df44358015
Tony Gilkerson <tgilkerson@miletwo.us>
2023-02-10 22:11:52 -0500

    add load-k8s-config.md

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ set -x
 # Put in path so "source load-k8s-config.sh" can be added in the .bashrc
 #
 install kubeconf/load-k8s-config.sh /usr/local/bin/
+install kubeconf/load-k8s-config.md $HOME/.kube/
 
 install .my-ps1 ~/
 install .my-bashrc ~/
---
+++ kubeconf/load-k8s-config.md
@@ -0,0 +1,7 @@
+# Kube Config
+
+To combine all config files run the following: See `~/github/dotfiles` for more info.
+
+```sh
+source load-k8s-config.sh
+```
