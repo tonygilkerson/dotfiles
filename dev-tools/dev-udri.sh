4df5d279f38c22c9b893859bd2989e85604a5c19
Tony E Gilkerson <tony.gilkerson@udri.udayton.edu>
2025-02-19 09:35:22 -0500

    wip

---
+++ dev-tools/dev-udri.sh
@@ -0,0 +1,8 @@
+# Some stuff that I install manually on my dev machine
+
+#
+# ghostty terminal
+#
+dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+dnf install ghostty
+
