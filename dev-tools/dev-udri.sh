9ec78b29c375adc153eafe8466ea3f6a61305d7d
Tony Gilkerson <tonygilkerson@yahoo.com>
2025-09-02 10:59:26 -0400

    wip

--- dev-tools/dev-udri.sh
+++ dev-tools/dev-udri.sh
@@ -1,9 +1,13 @@
 # Some stuff that I install manually on my dev machine
 
+sudo apt update
+sudo apt install vim
+
 # Install homebrew
 su  do apt update
 sudo apt install build-essential git -y
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 
 brew install yq 
 brew install dagger/tap/dagger
